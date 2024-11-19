ed6fbc88d9a80c537b650b8dce3d96efd8120ae5
XavierQu23 <quishpexavier27@gmail.com>
2024-11-18 23:59:26 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,14 +1,7 @@
+require 'sinatra'
 require 'webrick'
-
-server = WEBrick::HTTPServer.new(Port: 5050)
-
-server.mount_proc '/' do |req, res|
-  res['Content-Type'] = 'text/html'
-  res.body = '<html><body><h1>Hola  mundo en rubi </h1></body></html>'
+get '/' do
+  '<html><body><h1>Hola mundo en Ruby</h1></body></html>'
 end
-
-trap('INT') { server.shutdown }
-
-server.start
 set :bind, '0.0.0.0'
 set :port, 5050
